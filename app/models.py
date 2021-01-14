efa14b8140a1d5e5cdfb6d47a1b3fa7fa2e3aa5a
Seth <switfoth@gmail.com>
2021-01-13 18:21:05 -0600

    Hit by the Joe EoD Mixup

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 
 class Employee(db.Model, UserMixin):
+    __tablename__ = "employees"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     employee_number = db.Column(db.Integer, unique=True)
@@ -24,3 +25,23 @@ class Employee(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+class Menu(db.Model):
+    __tablename__ = "menus"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+
+class MenuItem(db.Model):
+    __tablename__ = "menu_items"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    price = db.Column(db.Float)
+    menu_id = db.Column(db.Integer, db.ForeignKey("menus.id"))
+    menu_type_id = db.Column(db.Integer, db.ForeignKey("menu_item_types.id"))
+    menu = db.relationship("Menu")
+    type = db.relationship("MenuItemType")
+
+class MenuItemType(db.Model):
+    __tablename__ = "menu_item_types"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
--- database.py
+++ database.py
@@ -6,7 +6,7 @@ load_dotenv()
 # so that the environment variables are
 # properly loaded.
 from app import app, db
-from app.models import Employee
+from app.models import Employee, Menu, MenuItem, MenuItemType
 
 
 with app.app_context():
@@ -15,4 +15,22 @@ with app.app_context():
 
     employee = Employee(name="Margot", employee_number=1234, password="password")
     db.session.add(employee)
+
+    beverages = MenuItemType(name="Beverages")
+    db.session.add(beverages)
+    entrees = MenuItemType(name="Entrees")
+    db.session.add(entrees)
+    sides = MenuItemType(name="Sides")
+    db.session.add(sides)
+
+    dinner = Menu(name="Dinner")
+    db.session.add(dinner)
+
+    fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
+    db.session.add(fries)
+    drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
+    db.session.add(drp)
+    jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
+    db.session.add(jambalaya)
+
     db.session.commit()
